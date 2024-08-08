4b03930436b1daabc8fddc1a6b22102059458f66
hudsonhok <hlhok4@gmail.com>
2024-08-08 10:31:38 -0400

    Add base url to settings

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_URL = os.environ.get("BASE_URL")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
